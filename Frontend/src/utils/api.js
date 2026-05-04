82da6f45612cb69b4373ad724ac4a1124f491521
Amarsah15 <gsah150803@gmail.com>
2026-05-04 18:53:07 +0530

    Add comment for development environment baseURL in api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// For development environment baseURL: "http://localhost:8000/api/v1",
+
 export const axiosInstance = axios.create({
   baseURL: "https://connectify-wjv8.onrender.com/api/v1",
   withCredentials: true,
